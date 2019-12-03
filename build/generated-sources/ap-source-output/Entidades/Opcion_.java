fa72ba1f52ac76d182286d9aa35f6a1f0f0fffd3
Isaac8323 <55283588+Isaac8323@users.noreply.github.com>
2019-12-03 11:00:03 -0600

    Bar Graphic Bean
    Se añadió el bean correspondiente para la correcta visualización de las gráficas de barras

--- build/generated-sources/ap-source-output/Entidades/Opcion_.java
+++ build/generated-sources/ap-source-output/Entidades/Opcion_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-02T22:16:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-03T09:42:45")
 @StaticMetamodel(Opcion.class)
 public class Opcion_ { 
 
